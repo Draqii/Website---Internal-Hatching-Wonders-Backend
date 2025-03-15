289e4cf31b00f345336c046ced32e326d5231ea1
Draq <silentdraqon@gmail.com>
2025-03-15 02:14:03 +0100

    oki

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -14,7 +14,7 @@ kittySchema.methods.speak = function speak() {
     console.log(greeting);
 };
 
-/* GET home page. */
+
 router.post('/subscribe', async function (req, res, next) {
     try {
     const payload = req.body.payload
@@ -28,6 +28,25 @@ router.post('/subscribe', async function (req, res, next) {
         console.log(err)
     }
     res.json({ status: "hewo" });
+});
+
+
+router.post('/get-subscribers', async function (req, res, next) {
+    try {
+    const payload = req.body.payload
+    console.log(payload)
+    await mongoose.connect(connection_string);
+    const Kitten = mongoose.model('NewsletterSubscriber', kittySchema);
+    const kitten = await Kitten.find()
+    res.json({ status: "hewo",
+        payload: {
+            subscribers: kitten
+        }
+     });
+    }
+    catch (err) {
+        console.log(err)
+    }
 });
 
 module.exports = router;
